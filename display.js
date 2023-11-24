4e91933275c57e58ea170fc588c80b07f625deb5
sarahOmar <117687177+codeQueen211@users.noreply.github.com>
2023-11-24 19:05:37 +0200

    Add files via upload

---
+++ display.js
@@ -0,0 +1,24 @@
+function scrollToSection() {
+    // Get the section element by its ID
+    const section = document.getElementById('section1'); 
+    // Scroll to the section
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToContact() {
+    // Get the section element by its ID
+    const section = document.getElementById('section2'); 
+    // Scroll to the section
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToEdu() {
+    // Get the section element by its ID
+    const section = document.getElementById('section3'); 
+    // Scroll to the section
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToSkills() {
+    // Get the section element by its ID
+    const section = document.getElementById('section4'); 
+    // Scroll to the section
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
